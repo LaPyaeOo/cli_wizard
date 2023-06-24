e18ac9f7a9908c0deaed129d16a5496fedf2bb78
la pyae <lapyaeoo@ayainnovation.com>
2023-06-25 02:43:38 +0630

    add: collectInformation method in terminal

--- lib/src/terminal.dart
+++ lib/src/terminal.dart
@@ -4,4 +4,16 @@ class Terminal {
   void printPromp(String prompt) {
     stdout.writeln(prompt);
   }
+
+  void clearScreen() {
+    if (Platform.isWindows) {
+      stdout.write('\x1B[2J\x1B[0f');
+    } else {
+      stdout.write('\x1B[2J\x1B[3J\x1B[H');
+    }
+  }
+
+  String collectInformation() {
+    return stdin.readLineSync();
+  }
 }
